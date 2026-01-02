f20b13220750b8921f8558d41c7a17b326a77849
Edward Rosten <ed@edwardrosten.com>
2026-01-02 17:14:39 +0000

    32 points on reshi data

--- train_nupc.py
+++ train_nupc.py
@@ -247,8 +247,8 @@ def PredictReconstructionCrazy(model_size: int, extra_points: int, nm_per_pixel_
 
 
 def _main()->None:
-    #nupc3d = [t.to(device.device).half() for t in resi_data.load_3d()]
-    nupc3d = [t.to(device.device).half() for l in mark_bates_data.load_3d_list() for t in l]
+    nupc3d = [t.to(device.device).half() for t in resi_data.load_3d()]
+    #nupc3d = [t.to(device.device).half() for l in mark_bates_data.load_3d_list() for t in l]
 
     initial_points=32
     mult = 1
