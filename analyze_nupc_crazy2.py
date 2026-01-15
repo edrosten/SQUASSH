984438d330fb8d48c953b44234a1379cf597ba82
Edward Rosten <ed@edwardrosten.com>
2026-01-15 16:21:54 +0000

    Clean up the code a little

--- analyze_nupc_crazy2.py
+++ analyze_nupc_crazy2.py
@@ -20,17 +20,20 @@ from localisation_data import LocalisationDataSetMultipleDan6
 from train_nupc import PredictReconstruction, AxialStretchRadialExpandWithGeneralShift
 
 
-SCALE=1.3
-
+## Standard parameters for the end of an NUPC run
 data_parameters = train.DataParametersXYYZ(
     image_size_xy = 64,
     image_size_z = 32,
-    nm_per_pixel_xy = 3*SCALE,
+    nm_per_pixel_xy = 3.9,
     z_scale = 2
 )
+final_fwhm = 13.0
 
-final_fwhm = SCALE * 10.0
 
+# General parameters for figure plotting
+# Plotting with everything twice the size improves the figure a bit
+# because the margins don't get scaled up, so the final figure looks
+# a bit tighter and wastes less space 
 FIGSCALE=2
 cm = FIGSCALE/2.54  # centimeters in inches, plus an overall figure scaling
 FS=7*FIGSCALE
@@ -40,28 +43,54 @@ def _load_net(nupc3d: list[Tensor], trained_weights: dict, pts:int)->tuple[Gener
     net, parameterisation = PredictReconstruction(initial_model_size=pts, final_model_size=pts, **vars(data_parameters), data=nupc3d)
     parameterisation.per_point_shift=True
     
+    # _orig is an artifact of how torch.compile() works. The emitted network references
+    # the underlying parameters by prepending 10 characters of stuff. Strip them here since
+    # sometimes I forgot to re-enable torch.compile so not every saved net has these.
     if "_orig" in next(iter(trained_weights.keys())):
         trained_weights = { k[10:]:v for k,v in trained_weights.items()}
+
+    # During the development, shift_network went from private to public, so this 
+    # code allows loading old runs with the private shift network
     trained_weights = { k.replace("_shift_network", "shift_network"):v for k,v in trained_weights.items()}
 
     net.load_state_dict(trained_weights)
-
+    
+    # Switch off everything related to training here
     net.eval()
     for i in net.parameters():
         i.requires_grad=False
         
     return net, parameterisation
 
+def _cov(points: Tensor)->Tensor:
+    assert len(points.shape)==3
+    pts_centred = points - points.mean(1).unsqueeze(1).expand_as(points)
+    return torch.einsum('hij,hik->hjk', pts_centred, pts_centred)/pts_centred.shape[1]
+
+def _primary_axis_angle(cov: Tensor)->Tensor:
+    assert len(cov.shape)==3
+    assert cov.shape[1] == 2
+    assert cov.shape[2] == 2
+    val, vec =torch.linalg.eigh(cov)  # pylint: disable=not-callable
+
+    assert (val[:,1] >= val[:,0]).all()
+    
+    major_axis = vec[:,:,1]
+    major_axis *= major_axis[:,1].sign().unsqueeze(1).expand(-1, 2)
+    return torch.atan2(major_axis[:,1], major_axis[:,0])
+
+
 @dataclass
 class _NetRes:
     points: Tensor
-    angles: Tensor
     indices: list[int]
-    eigs: Tensor
     images: list[list[Tensor]]
     data: list[list[Tensor]]
 
-def _apply_net_and_get_3d_points(nupc3d: list[Tensor], trained_weights: dict, pts:int=700)->_NetRes:
+
+
+
+def _apply_net(nupc3d: list[Tensor], trained_weights: dict, pts:int=700)->_NetRes:
 
     final_sigma = train.fwhm_to_sigma(final_fwhm)
     final_sigma_t = torch.tensor(final_sigma)
@@ -80,8 +109,6 @@ def _apply_net_and_get_3d_points(nupc3d: list[Tensor], trained_weights: dict, pt
 
     pts_list = []
     ind_list = []
-    ang_list = []
-    eig_list = []
     img_list = []
     data_list = []
 
@@ -106,26 +133,9 @@ def _apply_net_and_get_3d_points(nupc3d: list[Tensor], trained_weights: dict, pt
 
             points = points.squeeze().cpu()
 
-
-            pts_centred = points - points.mean(0).unsqueeze(0).expand_as(points)
-
-
-            cov2d = torch.einsum('ij,ik->jk', pts_centred[:,0:2], pts_centred[:,0:2])/pts_centred.shape[0]
-            val, vec =torch.linalg.eigh(cov2d)  # pylint: disable=not-callable
-
-            assert val[1] >= val[0]
-            
-            major_axis = vec[:,1]
-            major_axis *= major_axis[1].sign()
-            angle = torch.atan2(major_axis[1], major_axis[0])
-
-
-
             if is_valid > 0.5:
                 pts_list.append(points.cpu())
                 ind_list.append(index)
-                ang_list.append(angle)
-                eig_list.append(val)
                 img_list.append(imgs)
                 data_list.append(datum)
 
@@ -155,13 +165,12 @@ def _apply_net_and_get_3d_points(nupc3d: list[Tensor], trained_weights: dict, pt
                     pts_px = points[:,0:2] / data_parameters.nm_per_pixel_xy + data_parameters.image_size_xy/2
                     plt.scatter(pts_px[:,0], pts_px[:,1], c=[.5, .5, 1, .02])
 
-                    center_2d_px = pts_px.mean(0)
-
-                    
-                    for i in [0,1]:
-                        v = vec[:,i] * val[i].sqrt() / data_parameters.nm_per_pixel_xy
-                        v = torch.stack([-v, v], 0) + center_2d_px.unsqueeze(0).expand(2,2)
-                        plt.plot(*v.permute(1,0))
+                    #center_2d_px = pts_px.mean(0)
+                    #
+                    #for i in [0,1]:
+                    #    v = vec[:,i] * val[i].sqrt() / data_parameters.nm_per_pixel_xy
+                    #    v = torch.stack([-v, v], 0) + center_2d_px.unsqueeze(0).expand(2,2)
+                    #    plt.plot(*v.permute(1,0))
 
 
                     #plt.axis('square')
@@ -175,9 +184,7 @@ def _apply_net_and_get_3d_points(nupc3d: list[Tensor], trained_weights: dict, pt
 
     return _NetRes(
         points=torch.stack(pts_list, 0), 
-        angles=torch.stack(ang_list, 0), 
         indices=ind_list, 
-        eigs=torch.stack(eig_list, 0),
         images=img_list,
         data=data_list,
     )
@@ -185,15 +192,15 @@ def _apply_net_and_get_3d_points(nupc3d: list[Tensor], trained_weights: dict, pt
 
 
 
-
-
-
-plot_size = 31397.046857833866
-
-
 def _plot_angular(good_means: Tensor, inp: _NetRes)->None:
-    angs = inp.angles
 
+    # This is a figure size that's big enough for the RESI data.
+    # Use it for both to they are a consistent size
+    plot_size = 31397.046857833866
+    
+    angs = _primary_axis_angle(_cov(inp.points[:,:,0:2]))
+    eigs, _ = torch.linalg.eigh(_cov(inp.points[:,:,0:2])) # pylint: disable=not-callable
+    
     plt.subplots(figsize=(8*cm, 4*cm))
     plt.clf()
     plotax = plt.gcf().subplot_mosaic("""
@@ -227,14 +234,14 @@ def _plot_angular(good_means: Tensor, inp: _NetRes)->None:
     plt.tight_layout()
 
 
-    #Create histogram
+    #Create angulat histogram as an inset plot
     plt.subplot(3,3,1, projection='polar')
     # x, y, w, h
     plt.gca().set_position(matplotlib.transforms.Bbox.from_bounds(.66, .3, .25, .25))
+
     # Create angular histogram
     counts, edges = np.histogram(angs, 20)
     for i, count in enumerate(counts):
-
         midangle = sum(edges[i:i+2])/2
         color = matplotlib.cm.twilight(midangle/torch.pi) # type: ignore[attr-defined] # pylint: disable=no-member
         plt.fill_between([edges[i], edges[i], edges[i+1], edges[i+1]], [0, count, count, 0], 0, color=color)
@@ -245,7 +252,7 @@ def _plot_angular(good_means: Tensor, inp: _NetRes)->None:
 
     # Show some image clips
     # Sort by aspect ratio to find some extreme examples
-    _, inds = (inp.eigs[:,1] / inp.eigs[:,0]).sort(descending=True)
+    _, inds = (eigs[:,1] / eigs[:,0]).sort(descending=True)
     
     # Pick the top 20
     best: list[int] = list(inds[0:20])
@@ -260,17 +267,18 @@ def _plot_angular(good_means: Tensor, inp: _NetRes)->None:
     for i in [0,1,2]:
         plt.sca(plotax[str(i)])
         plt.imshow((inp.data[selection[i]][0]+inp.data[selection[i]][1]).cpu().squeeze(), cmap='gray', origin='lower')
-        plt.title(f'{inp.angles[selection[i]]*180/torch.pi:.0f}°')
+        plt.title(f'{angs[selection[i]]*180/torch.pi:.0f}°')
         plt.axis('off')
         
 
     plt.pause(.1)
+    # This hacking here draws lines from the image clip to the point that the clip corresponds to.
     transFigure = plt.gcf().transFigure.inverted()
     lines = []
     for n, i in enumerate(selection):
 
         coord1 = transFigure.transform(plotax['A'].transData.transform(good_means[i,0:2]))
-        coord2 = transFigure.transform(plotax[str(n)].transData.transform([64,32]))
+        coord2 = transFigure.transform(plotax[str(n)].transData.transform([64,32])) # 64,32 is midway down the right hand edge, since the image is 64x64
         lines.append(matplotlib.lines.Line2D((coord1[0],coord2[0]),(coord1[1],coord2[1]), transform=plt.gcf().transFigure))
 
     plt.gcf().lines = lines
@@ -290,7 +298,7 @@ def _plot_angular(good_means: Tensor, inp: _NetRes)->None:
 nupc3d_resi, nupc3d_resi_means = resi_data.load_3d_with_means()
 nupc3d_resi = [t.to(device.device).half() for t in resi_data.load_3d()]
 trained_weights_resi = torch.load('log/1766516868-66b60604c41adb3c784b829cbd0205da1b12c1cd/phase_2/final_net.zip', map_location=torch.device('cpu'))
-res_resi = _apply_net_and_get_3d_points(nupc3d_resi, trained_weights_resi)
+res_resi = _apply_net(nupc3d_resi, trained_weights_resi)
 good_means_resi = torch.stack(nupc3d_resi_means)[res_resi.indices,:]
 
 plt.close('all')
@@ -303,7 +311,7 @@ I=0
 
 nupc3d_bates, nupc3d_bates_means = mark_bates_data.load_3d_list_and_means()
 trained_weights_bates = torch.load('log/1766605809-a396351dc2c407f97a32efa35b421a9aa8d2de55/phase_2/final_net.zip', map_location=torch.device('cpu'))
-res_bates = _apply_net_and_get_3d_points(nupc3d_bates[I], trained_weights_bates)
+res_bates = _apply_net(nupc3d_bates[I], trained_weights_bates)
 good_means_bates = torch.stack(nupc3d_bates_means[I])[res_bates.indices,:]
 
 plt.close('all')
