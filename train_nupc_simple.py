e5063f3b0ebbc4e812cf7f92707e05facc77c688
Edward Rosten <edward.rosten@gmail.com>
2026-03-14 11:25:04 +0000

    Improve train_nupc_simple.py (#17)
    Make the training schedule match train_nupc_simple.ipynb.
    
    Now it successfully gets the axis with expand enabled.
    
    Also add some post-training analysis code to display the relative stretch histogram.
    
    ---------
    
    Co-authored-by: Edward Rosten <ed@edwardrosten.com>

--- train_nupc_simple.py
+++ train_nupc_simple.py
@@ -1,9 +1,15 @@
-# cast allows us to cast to specific types
+# cast needed to tell the type checker what a specific type is, since torch.compile returns a very generic type
 from typing import cast
+
+# Used for nice status display
+from tqdm import tqdm
+
+# Used for plotting
+import matplotlib.pyplot as plt
 
 # importing deep learning packages
 import torch
-import torch._dynamo
+from torch.utils.data import DataLoader
 
 # these are the imports for the data
 # you will need to adapt one of these if you want to import your own data
@@ -17,6 +23,44 @@ import device
 
 # this specifies the type of 3D representation that will be used (this will vary between SMLM data and others)
 import localisation_data
+
+
+def analyze_data(net: network.GeneralPredictReconstruction, parameterisation: network.ModelParameterisation, dataset: localisation_data.GeneralLocalisationDataSet)->tuple[torch.Tensor, torch.Tensor]:
+    '''Runs data through the network. Returns the scales and axes of valid data'''
+    loader = DataLoader(dataset, batch_size=1, shuffle=False)
+
+    # We need to pass the rendering sigma into the network. This needs to 
+    # be as a tensor on the correct device
+    fwhm_t = torch.tensor(13.0).to(device.device)
+
+    scales = []
+    axes = []
+
+    net.eval()
+    with torch.no_grad():
+        for batch in tqdm(loader):
+            # net() runs the network and produces a rendered image output. We want to analyze the 
+            # results, not just images, so we use process_input which outputs all the intermediate
+            # data including logits for the parameterisation
+            _,r,_,is_valid,parameters = net.process_input(batch, min_sigma_nm=localisation_data.fwhm_to_sigma(fwhm_t))
+
+            # Compute various things from the parameterisation
+            scale = parameterisation.compute_scale_from_parameters(parameters).cpu().item()
+            expand = parameterisation.compute_expand_from_parameters(parameters).cpu().item()
+            
+            # Reject invalid data from the network. Also, a number of NPCs have only one visible ring 
+            # which the network will reproduce by outputting strong in-axis squash in an attempt to 
+            # merge the two rings together. These NPCs are not useful to analyze.
+            if is_valid > 0.5 and scale > 0.8:
+                scales.append([scale, expand])
+                
+                # Also record the direction of the axis as it appears in image space. This is so we
+                # can identify which ring has positive Z and which has negative Z
+                axes.append((r @ parameterisation.get_axis().unsqueeze(1)).squeeze())
+
+    return torch.tensor(scales), torch.stack(axes, 0)
+
+
 
 
 def _main()->None:
@@ -56,22 +100,27 @@ def _main()->None:
     # The learning rate is set to allow faster optimisation at the beginning of the run, when gradients are likely to be steeper.
     # A lower learning rate can be used for the whole optimisation at the cost of a longer run time.
 
-    params_initial.schedule[0].epochs = 1500
+    params_initial.schedule[0].epochs = 90
     params_initial.schedule[0].initial_psf = 65 # in units of nm
-    params_initial.schedule[0].final_psf = 13 # in units of nm
-    params_initial.schedule[0].psf_step_every= 100
+    params_initial.schedule[0].final_psf = 33.4 # in units of nm
+    params_initial.schedule[0].psf_step_every= 30
     params_initial.schedule[0].initial_lr= 0.0001
-    params_initial.schedule[0].final_lr= 0.00005
+    params_initial.schedule[0].final_lr= 0.0001
 
+    params_initial.schedule.append(train.TrainingSegment())
+    params_initial.schedule[1].epochs = 900
+    params_initial.schedule[1].initial_psf = 24.7
+    params_initial.schedule[1].final_psf = 13
+    params_initial.schedule[1].psf_step_every= 100
+    params_initial.schedule[1].initial_lr= 0.0001
+    params_initial.schedule[1].final_lr= 0.0001
     
     # DataSet6Plane defines how the data will be rendered, this was used for all SMLM data
     # If you wish to use non-SMLM data you will need to change to a different renderer 
-    dataset_initial = localisation_data.DataSet6Plane(**vars(data_parameters), data=nupc3d, augmentations=1, device=device.device)
+    dataset = localisation_data.DataSet6Plane(**vars(data_parameters), data=nupc3d, augmentations=1, device=device.device)
 
 
-    # torch dynamo optimises speed performance
     # resetting the torch compiler is advisable as it is occasionally prone to writing data into the compiled code if this is not done
-    torch._dynamo.config.cache_size_limit=512  # pylint: disable=protected-access
     torch.compiler.reset()
 
     # This sets the size of the model and the parameters of the heterogeneity
@@ -81,8 +130,37 @@ def _main()->None:
     net.to(device.device)
     
     fast = cast(network.GeneralPredictReconstruction, torch.compile(net))
-    train.retrain(fast, dataset_initial, params_initial, 'phase_0')
+    train.retrain(fast, dataset, params_initial, 'phase_0')
     
+
+
+    # Second training phase
+    #
+    # At this point the system should have found the structure pretty well and
+    # the axis of stretch. To refine the structure, we allow expansion in the
+    # parameterisation and we continue the training at the finest scale with a
+    # decreasing learning rate. 
+    params_final = train.TrainingParameters()
+    params_final.batch_size = 10
+    params_final.validity_weight=rejection
+    params_final.schedule[0].epochs = 1000
+    params_final.schedule[0].initial_psf = 13
+    params_final.schedule[0].final_psf = 13
+    params_final.schedule[0].psf_step_every= 300
+    params_final.schedule[0].initial_lr= 0.0001
+    params_final.schedule[0].final_lr= 0.0005
+    parameterisation.max_stretch_factor_expand = 1.3
+    torch.compiler.reset()
+    train.retrain(fast, dataset, params_final, 'phase_1')
+
+    # Do some data plotting
+    scales, _ = analyze_data(net, parameterisation, dataset)
+    plt.hist(scales[:,0], 30)
+    plt.xlabel('Relative scaling of the underlying model in Z')
+    plt.ylabel('Count')
+    plt.show()
+
+
         
 if __name__ == "__main__":
     _main()
